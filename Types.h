b28fd6d33e5c8793c9e2755b90fa5ada77adb391
Faraz2010 <s3754395@student.rmit.edu.au>
2020-09-18 04:06:04 +1000

    [chores] tidying up types.

--- Types.h
+++ Types.h
@@ -1,10 +1,10 @@
 // Enum list of available tiles
 enum Tile {
-    RED = 'R',
-    YELLOW = 'Y',
-    DARK_BLUE = 'B',
-    LIGHT_BLUE = 'L',
-    BLACK = 'U',
-    FIRST_PLAYER = 'F',
-    NO_TILE = '.'
+    TILE_RED = 'R',
+    TILE_YELLOW = 'Y',
+    TILE_DARK_BLUE = 'B',
+    TILE_LIGHT_BLUE = 'L',
+    TILE_BLACK = 'U',
+    TILE_FIRST_PLAYER = 'F',
+    TILE_NO_TILE = '.'
 };
