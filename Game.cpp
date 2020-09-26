54b1cc0d68a48c7c255858b733ec6a455bf0af29
Faraz2010 <s3754395@student.rmit.edu.au>
2020-09-27 04:16:50 +1000

    feat/game

--- Game.cpp
+++ Game.cpp
@@ -1,5 +1,21 @@
-// Faraz
+#include "Game.h"
 
+// Faraz
+bool Game::playRound(int factory, Tile tile, int bag)
+{
+	Factory* f = factories.at(factory);
+	Player* player = players.at(activePlayer);
+
+	std::vector<Tile> tiles = f->pullTiles(tile, factories.at(0));
+	if (tiles.size() == 0)
+		return false;
+	
+	// TODO: play
+
+	activePlayer = getNextPlayerIndex();
+	return true;
+	//int activeIndex = (activePlayer + players.size() - firstPlayer) % players.size(); (getNextPlayer)
+}
 
 // Manav
 
